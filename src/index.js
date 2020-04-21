7793e8add15ac9f52621899a1dcc97f75039de99
DavidColatti <davidcolatti@gmail.com>
2020-04-21 19:13:44 -0400

    modified

--- src/index.js
+++ src/index.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
 class App extends React.Component {
-    constructor(props) {
-        super(props)
+	constructor(props) {
+		super(props);
 
-        this.state = {
-            lat: null
-        }
-    }
-    
+		this.state = {
+			lat: null
+		};
+
+		window.navigator.geolocation.getCurrentPosition(
+			(position) => {
+				this.setState({
+					lat: position.coords.latitude
+				});
+			},
+			(err) => console.log(err)
+		);
+	}
+
 	render() {
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err));
-
-		return <div>Latitude: </div>;
+		return <div>Latitude: {this.state.lat}</div>;
 	}
 }
 
