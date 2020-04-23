f905b4fff9471519c510a6acd6a463b89685d0eb
David Colatti <davidcolatti@gmail.com>
2020-04-23 18:45:27 -0400

    conditional rendering

--- src/index.js
+++ src/index.js
@@ -6,21 +6,35 @@ class App extends React.Component {
 		super(props);
 
 		this.state = {
-			lat: null
+			lat: null,
+			errorMessage: ''
 		};
 
 		window.navigator.geolocation.getCurrentPosition(
 			(position) => {
+				console.log('got the latitude')
 				this.setState({
 					lat: position.coords.latitude
 				});
 			},
-			(err) => console.log(err)
+			(err) => {
+				this.setState({
+					errorMessage: err.message
+				})
+			}
 		);
 	}
 
 	render() {
-		return <div>Latitude: {this.state.lat}</div>;
+		if (this.state.errorMessage && !this.state.lat) {
+			return <div> Error: {this.state.errorMessage} </div>
+		}
+
+		if (!this.state.errorMessage && this.state.lat) {
+			return <div> Latitude: {this.state.lat} </div>
+		}
+
+		return <div>Loading!</div>
 	}
 }
 
